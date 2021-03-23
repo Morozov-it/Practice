e0504efb1ea927dfb07d9ec962dcd22b991f6782
Morozov-it <morozov.it2020@gmail.com>
2021-03-23 12:58:50 +0300

    1

---
+++ WEB/Business_page/js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	$('.icon-menu').click(function(event) {
+		$('.menu__icon,.menu__body').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
+
+function ibg(){
+	$.each($('.ibg'), function(index, val) {
+		if($(this).find('img').length>0){
+			$(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+		}
+	});
+}
+ibg();
+
+$('body').on('click', '.tab__navitem', function (event) {
+	var eq = $(this).index();
+	if ($(this).hasClass('parent')) {
+		var eq = $(this).parent().index();
+	}
+	if (!$(this).hasClass('active')) {
+		$(this).closest('.tabs').find('.tab__navitem').removeClass('active');
+		$(this).addClass('active');
+		$(this).closest('.tabs').find('.tab__item').removeClass('active').eq(eq).addClass('active');
+		if ($(this).closest('.tabs').find('.slick-slider').length > 0) {
+			$(this).closest('.tabs').find('.slick-slider').slick('setPosition');
+		}
+	}
+});
+
+
